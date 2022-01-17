8073753937a0f324bb0e0dc8dcc69a1367b6f6bb
Tokhab <tokhab@gmail.com>
2022-01-17 17:35:08 +0300

    Just start

---
+++ main.py
@@ -0,0 +1,4 @@
+import jmespath
+import json
+import requests
+
---
+++ temp.py
@@ -0,0 +1,19 @@
+import jmespath
+import json
+import requests
+
+# Получаем определенный элемент
+d = {"foo": {"bar-1": "baz"}}
+print(jmespath.search('foo.bar', d))
+# baz
+
+# С помощью подстановочного знака получаем все названия
+d = {"foo": {"bar": [{"name": "one"}, {"name": "two"}]}}
+print(jmespath.search('foo.bar[*].name', d))
+# [“one”, “two”]
+text = 'inquiry'
+respond = requests.get(f'https://dictionary.yandex.net/dicservice.json/lookupMultiple?ui=ru&srv=tr-text&text={text}&type=regular%2Csyn%2Cant%2Cderiv&lang=en-ru&flags=7591&dict=en-ru.regular%2Cen.syn%2Cen.ant%2Cen.deriv&yu=2048703991642255491&yum=1642255618671602095')
+# print(respond.json())
+# json_dict = json.loads(respond.json())
+# print(jmespath.search('en-ru', json_dict))
+print(respond.json()['en-ru']['regular'][0]['tr'][0]['text'])
