c346bf1b7a1076c3937d016b7b2e9de96dabb42d
Tokhab_w <tokhab@mail.ru>
2022-01-18 17:35:36 +0300

    I've just finished add_data and translate

--- temp.py
+++ temp.py
@@ -2,24 +2,42 @@ import jmespath
 import json
 import requests
 
-def search_in_dict(source_dict=None, desired_key=None):
-    for i in source_dict.keys():
-        # if i == 'text':
-        print(i, '=', source_dict[i])
-        if isinstance(source_dict[i], dict):
-            search_in_dict(source_dict[i], desired_key)
-        elif isinstance(source_dict[i], list):
-            for ii in range(0, len(source_dict[i])):
-                if isinstance(source_dict[i][ii], dict):
-                    search_in_dict(source_dict[i][ii], desired_key)
+def translate(source_dict=None):
+    result = []
+    for i in range(0, len(source_dict['en-ru']['regular'])):
+        for ii in range(0, len(source_dict['en-ru']['regular'][i]['tr'])):
+            result.append(source_dict['en-ru']['regular'][i]['tr'][ii]['text'])
+    return result
+
+def add_data(source_dict=None, type_data=None):
+    result = []
+    for i in range(0, len(source_dict['en'][type_data])):
+        for ii in range(0, len(source_dict['en'][type_data][i]['tr'])):
+            result.append(source_dict['en'][type_data][i]['tr'][ii]['text'])
+    return result
 
 if __name__ == '__main__':
-    text = 'complete'
+    text = 'inquiry'
+    text = 'welcome'
+    text = ' complete'
     respond = requests.get(f'https://dictionary.yandex.net/dicservice.json/lookupMultiple?ui=ru&srv=tr-text&text={text}&type=regular%2Csyn%2Cant%2Cderiv&lang=en-ru&flags=7591&dict=en-ru.regular%2Cen.syn%2Cen.ant%2Cen.deriv&yu=2048703991642255491&yum=1642255618671602095')
-    search_in_dict(respond.json(), 2)
-    # # Получаем определенный элемент
-    # d = {"foo": {"bar-1": "baz"}}
-    # print(jmespath.search('foo.bar', d))
+    print(translate(respond.json()))
+    print(add_data(respond.json(), 'syn'))
+    print(add_data(respond.json(), 'deriv'))
+    print(add_data(respond.json(), 'ant'))
+
+    text1 = 'after consideration'
+    respond1 = requests.get(f'https://dictionary.yandex.net/dicservice.json/lookupMultiple?ui=ru&srv=tr-text&text={text1}&type=syn%2Cant%2Cderiv%2Cregular&lang=en-ru&flags=1255&dict=en.syn%2Cen.ant%2Cen.deriv%2Cen-ru.regular&yu=2048703991642255491&yum=1642255618671602095')
+    print(translate(respond1.json()))
+    print(add_data(respond1.json(), 'syn'))
+    print(add_data(respond1.json(), 'deriv'))
+    print(add_data(respond1.json(), 'ant'))
+
+
+
+
+    # for i in range(0, len(respond.json()['en-ru']['regular'])):
+    #     translate(respond.json()['en-ru']['regular'][i]['tr'])
     # # baz
     #
     # # С помощью подстановочного знака получаем все названия
